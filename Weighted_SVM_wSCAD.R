7302a73e68a8bb47f3fd035f4bc12fa5d27801e6
Cheng-Han (Aaron) Yang <97812515+cyang728@users.noreply.github.com>
2023-07-10 00:56:20 -0500

    Update Weighted_SVM_wSCAD.R
    Use the original BIC instead of the weighted BIC

--- Weighted_SVM_wSCAD.R
+++ Weighted_SVM_wSCAD.R
@@ -75,12 +75,12 @@ scadsvc <- function(lambda1 = 0.01, x, y, a = 3.7, tol = 10^(-4), sample.weights
     # D = 1/(2*ntrain) * diag(1/res)
     # ###
     # save as a vector D_vec
-    D_vec <- wt / (2 * ntrain) * (1 / res)
+    D_vec <- sample.weights / (2 * ntrain) * (1 / res)
     aw <- abs(w)
     dp <- lambda1 * (aw <= lambda1) + (a * lambda1 - aw) / (a - 1) * (lambda1 < aw & aw <= a * lambda1)
     Q1_vec <- c(0, dp / aw)
     
-    P <- 0.5 * t(ytrain + y0) %*% diag(wt) %*% x1 / ntrain
+    P <- 0.5 * t(ytrain + y0) %*% diag(sample.weights) %*% x1 / ntrain
     
     # ###
     # Q1 = diag( c(0, dp/aw))
@@ -127,7 +127,8 @@ scadsvc <- function(lambda1 = 0.01, x, y, a = 3.7, tol = 10^(-4), sample.weights
     qx <- .calc.mult.inv_Q_mat3(U = t(x1), D_vec = D_vec, A_vec = Q1_vec, mat2 = t(x1), n.thr = 10^4)
     xqx <- 0.5 * as.matrix(x1) %*% qx
     
-    BIC <- sum(wt * pmax(0, 1 - ytrain * f)) + length(xind) * log(sum(wt))
+    #BIC <- sum(sample.weights * pmax(0, 1 - ytrain * f)) + length(xind) * log(sum(sample.weights))
+    BIC <- sum(pmax(0, 1 - ytrain*f)) + length(xind)*log(length(ytrain))
     
     # Output:
     #   w : direction vector of separating hyperplane
@@ -142,57 +143,4 @@ scadsvc <- function(lambda1 = 0.01, x, y, a = 3.7, tol = 10^(-4), sample.weights
   } else {
     return(-1)
   }
-}
-
-.calc.mult.inv_Q_mat3 <- function(U, D_vec, A_vec, mat2 = NULL, n.thr = 500) {
-  if (is.null(mat2)) mat2 <- diag(1, nrow = nrow(U), ncol = nrow(U))
-  
-  if (nrow(U) < n.thr) {
-    Q <- U %*% diag(D_vec) %*% t(U) + diag(A_vec)
-    
-    if (rank.condition(Q)$rank == nrow(Q) & nrow(Q) == ncol(Q)) {
-      inv_Q <- solve(Q)
-    } else {
-      inv_Q <- pseudoinverse(Q)
-    }
-    
-    if (all(is.na(inv_Q))) {
-      nbw <- .calc.mult.inv_Q_mat2(U = U, D_vec = D_vec, A_vec = A_vec, mat2 = mat2, n.thr = 0)
-    } else {
-      nbw <- inv_Q %*% mat2
-    }
-  } else {
-    if (all(A_vec == 0)) {
-      A <- diag(A_vec)
-      D <- diag(D_vec)
-      inv_U <- pseudoinverse(U)
-      
-      nbw <- t(inv_U) %*% (ginv(D_vec) %*% (inv_U %*% mat2))
-    } else {
-      eps <- 10^-8
-      A1_vec <- A_vec
-      A1_vec[A1_vec == 0] <- eps
-      inv_A1_vec <- 1 / A1_vec
-      
-      nbw.term1 <- vecmat(inv_A1_vec, mat2)
-      
-      tmp.mat <- ginv(diag(D_vec)) + matvec(t(U), inv_A1_vec) %*% U
-      
-      if (rank.condition(tmp.mat)$rank == nrow(tmp.mat) & nrow(tmp.mat) == ncol(tmp.mat)) {
-        try(tt1 <- solve(tmp.mat))
-      }
-      
-      if (!exists("tt1")) try(tt1 <- pseudoinverse(tmp.mat))
-      
-      if (!exists("tt1")) try(tt1 <- ginv(tmp.mat))
-      
-      if (!exists("tt1")) stop("Error: can not calculate inverse matrix for 'diag(1/D_vec) + matvec(t(U), inv_A1_vec )%*% U'")
-      
-      nbw.term2 <- vecmat(inv_A1_vec, U) %*% (tt1 %*% (matvec(t(U), inv_A1_vec) %*% mat2))
-      
-      nbw <- nbw.term1 - nbw.term2
-    }
-  }
-  
-  return(nbw)
 }
